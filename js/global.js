5b12a9cc97020845f7586999cdfd552d58738a14
borga <borgaaydin@gmail.com>
2017-04-16 14:37:54 +0300

    urun detay

--- js/global.js
+++ js/global.js
@@ -23,5 +23,33 @@ $(function() {
         slidesToScroll: 5
     });
 
+    $(".product_detail_slider").slick({
+        arrows: true,
+        variableWidth: true,
+        infinite: false,
+        dots: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5
+    });
+
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        focusOnSelect: true,
+        vertical: true,
+        arrows: false
+    });
 
 });
