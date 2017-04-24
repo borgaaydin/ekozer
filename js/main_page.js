f3aefad61320a826c89b47e1f04c88994904f262
borga <borgaaydin@gmail.com>
2017-04-24 20:48:31 +0200

    js split

---
+++ js/main_page.js
@@ -0,0 +1,21 @@
+$(function() {
+    $(".main_slider").slick({
+        arrows: true,
+        variableWidth: true,
+        infinite: false,
+        dots: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+
+    $(".product_slider").slick({
+        arrows: true,
+        variableWidth: true,
+        infinite: false,
+        dots: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5
+    });
+});
