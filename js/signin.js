76158ef3159d69b25c84233d9df75acc0b1384a0
ukaytac <utku.aytac94@gmail.com>
2017-04-24 23:39:35 +0300

    Sign In

---
+++ js/signin.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var uyeSifre = $("#uyeSifre");
+    var change = "";
+    $(".uyeSifre .eye").click(function(){
+        change = "password";
+        $(this).hide();
+        var rep = $("<input type='" + change + "' />")
+            .attr("id", uyeSifre.attr("id"))
+            .attr("name", uyeSifre.attr("name"))
+            .attr('class', uyeSifre.attr('class'))
+            .val(uyeSifre.val())
+            .insertBefore(uyeSifre);
+        uyeSifre.remove();
+        uyeSifre = rep;
+        $(".uyeSifre .eye-closed").show();
+    }).insertAfter(uyeSifre);
+    $(".uyeSifre .eye-closed").click(function(){
+        change = "text";
+        $(this).hide();
+        var rep = $("<input type='" + change + "' />")
+            .attr("id", uyeSifre.attr("id"))
+            .attr("name", uyeSifre.attr("name"))
+            .attr('class', uyeSifre.attr('class'))
+            .val(uyeSifre.val())
+            .insertBefore(uyeSifre);
+        uyeSifre.remove();
+        uyeSifre = rep;
+        $(".uyeSifre .eye").show();
+    }).insertAfter(uyeSifre);
+});
