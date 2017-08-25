0a7af6ae12d14b03b1c905dd0cce113ec2cb8e99
borga <borgaaydin@gmail.com>
2017-08-25 12:39:04 +0200

    head accordion close on outside click

--- js/global.js
+++ js/global.js
@@ -1,13 +1,28 @@
 /**
  * Created by borga on 06/06/2017.
  */
-var display = false;
-$( ".logged-nav-button" ).click(function() {
-    display = !display;
-    if ( display === true ) {
-        $( ".logged-nav" ).addClass("open");
-    } else if ( display === false ) {
-        $( ".logged-nav" ).removeClass("open");
-        $(".accordion").accordion( "option", "active", false );
-    }
+
+$(function() {
+
+    var display = false;
+    var loggedNav = $(".logged-nav");
+    $(document).click(function(e) {
+        e.stopPropagation();
+
+        display = loggedNav.accordion("option", "active");
+        console.log("display : " + display);
+        console.log("display : " + $(".logged-nav-button").is(e.target));
+        console.log("display : " + loggedNav.is(e.target));
+        console.log(e.target);
+        if (!$(".logged-nav-button").is(e.target) && !loggedNav.is(e.target) && !$(".logged-nav-button > span").is(e.target)  && !$(".logged-nav-container").is(e.target) && (display === 0) ) {
+            e.stopPropagation();
+            loggedNav.accordion("option", "active", false);
+        }
+    });
+
+    $(".logged-nav-container").click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    })
+
 });
