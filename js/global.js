cae8d7eec02deb7c63e9986ed157233541168a35
borga <borgaaydin@gmail.com>
2017-08-25 12:43:28 +0200

    head accordion close on outside click

--- js/global.js
+++ js/global.js
@@ -8,12 +8,7 @@ $(function() {
     var loggedNav = $(".logged-nav");
     $(document).click(function(e) {
         e.stopPropagation();
-
         display = loggedNav.accordion("option", "active");
-        console.log("display : " + display);
-        console.log("display : " + $(".logged-nav-button").is(e.target));
-        console.log("display : " + loggedNav.is(e.target));
-        console.log(e.target);
         if (!$(".logged-nav-button").is(e.target) && !loggedNav.is(e.target) && !$(".logged-nav-button > span").is(e.target)  && !$(".logged-nav-container").is(e.target) && (display === 0) ) {
             e.stopPropagation();
             loggedNav.accordion("option", "active", false);
